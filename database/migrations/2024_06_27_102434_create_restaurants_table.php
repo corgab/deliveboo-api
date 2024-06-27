47ac8b489719738b2f893afd71bcacee278b71bf
corgab <corbanigabriele@gmail.com>
2024-06-27 14:33:09 +0200

    ADD: restaurant model + migration

---
+++ database/migrations/2024_06_27_102434_create_restaurants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+use App\Models\Restaurant;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',150);
+            $table->string('slug')->unique();
+            $table->string('address');
+            $table->string('vat', 11);
+            $table->string('thumb')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
