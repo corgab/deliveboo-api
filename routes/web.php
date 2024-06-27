354bc88c6bfebec7ecb96bcc54df4f355476a2fb
corgab <corbanigabriele@gmail.com>
2024-06-27 15:26:23 +0200

    ADD: Restaurants Routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\RestaurantController as AdminRestaurantController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,9 +21,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function(){
+        
+        Route::get('/', function () {
+            return view('admin.dashboard');
+        })->name('dashboard');
+        Route::resource('restaurants', AdminRestaurantController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
