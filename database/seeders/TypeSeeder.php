95a6b3e086d14a8bf044e4a62c1ca212b668eb2a
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-28 10:42:08 +0200

    Work to Public Funcrion with CSV file.

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,14 +4,43 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Type;
 class TypeSeeder extends Seeder
 {
+    public function getCSVData(string $path) {
+        $data = [];
+
+        $file_stream = fopen($path, 'r');
+
+        if ($file_stream === false) {
+            exit('fail' . $path);
+        }
+
+        while (($row = fgetcsv($file_stream)) !== false) {
+            $data[] = $row;
+        }
+
+        fclose($file_stream);
+
+        return $data;
+    }
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $data = $this->getCSVData(__DIR__.'/csv/types.csv');
+
+        foreach ( $data as $index=>$row) {
+            if ($index !== 0) {
+
+                $new_type = new Type();
+
+                $new_type->name = $row[0];
+
+                // Salvataggio dati 
+                $new_type->save();
+            }
+        }
     }
 }
