59193df8a8865603d2104d5760e58217e207ae17
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-28 17:39:59 +0200

    ADD: Edit login page user.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,25 +2,41 @@
 
 @section('content')
 <div class="container">
-    <h2 class="fs-4 text-secondary my-4">
+    <h2 class="fs-5 text-secondary my-4">
         {{ __('Dashboard') }}
     </h2>
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header">{{ __('Dashboard del ristoratore') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Sei correttamente loggato.') }}
                 </div>
             </div>
         </div>
     </div>
+    <section>
+        <div class="bg-warning rounded mt-5 py-4">
+            <h1 class="text-center fs-1 pt-5">Ciao e benvenuto in <span class="text-danger">Deliveboo</span>.
+            </h1>
+            <p class="my-4 fs-4 text-center px-5">Questa è la tua Dashboard. Da qui potrai sempre visionare il
+                <strong>menù</strong> e <strong>lista completa</strong> dei tuoi piatti, gli <strong>ordini
+                    ricevuti</strong> e le <strong>statistiche</strong> sugli ordini.
+            </p>
+
+            <div class="d-flex justify-content-center gap-5 mt-5 pb-5">
+                <a href="{{ route('admin.dishes.index') }}" class="btn btn-danger">Menù e Lista Piatti</a>
+                <a href="{{ route('admin.orders.index') }}" class="btn btn-danger">Ordini</a> 
+                <a href="{{ route('admin.orders.index') }}" class="btn btn-danger">Statistiche</a>  
+            </div>
+        </div>
+    </section>
 </div>
 @endsection
