106a403cab6b182770aaa9928d369228ee486efd
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-28 10:32:05 +0200

    Insert Public Function and protected with types.

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+
+    protected $table = 'types';
+    public function restaurants()
+    {
+        return $this->belongsToMany(Restaurant::class, 'restaurant_type');
+    }
 }
