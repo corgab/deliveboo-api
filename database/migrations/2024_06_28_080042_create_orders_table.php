8e349303bab368919cbd19159eac6cc9c1f25fcb
Davide-Morellato <morellato.davide@yahoo.it>
2024-06-29 09:59:23 +0200

    Delete: restaurant_id column into Orders Table

--- database/migrations/2024_06_28_080042_create_orders_table.php
+++ database/migrations/2024_06_28_080042_create_orders_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('restaurant_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('number',10);
