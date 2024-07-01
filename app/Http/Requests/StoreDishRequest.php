f5d387f479e6ef472d8fa29eeeeab1d58231daa6
Davide-Morellato <morellato.davide@yahoo.it>
2024-07-01 15:38:48 +0200

    Fix: visible StoreDishRequest

--- app/Http/Requests/StoreDishRequest.php
+++ app/Http/Requests/StoreDishRequest.php
@@ -25,7 +25,7 @@ class StoreDishRequest extends FormRequest
             'name' => 'required|max:100',
             'description_ingredients' => 'required',
             'price' => 'required',
-            'visible' => 'required|boolean',
+            'visible' => 'nullable|boolean',
             'thumb' => 'nullable'
         ];
     }
