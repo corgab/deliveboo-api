2c60d2cfc75afcd99f4d777387574d21635a0c83
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-07-01 11:41:56 +0200

    ADD: api-restaurant - show

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dish;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
     public function index(){
-        $restaurants = Restaurant::all();
+        $restaurants = Restaurant::all(); //paginate
         return response()->json([
-               'restaurants' => $restaurants
-               // success 
-         ]); 
+            'restaurants' => $restaurants
+            // 'success' => true 
+            // da valutare se usarlo o meno
+         ]);
+    }
+
+    public function show(Restaurant $restaurant){
+
+        $restaurant ->load( 'dishes' );
+
+        return response()->json([
+            'restaurant' => $restaurant
+        ]);
     }
 }
