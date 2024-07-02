7f8dcf98877d0ac52f1bbbc5f5a56a02b4e7c845
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-07-02 12:37:06 +0200

    ADD: create and edit index and show page.

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -1,22 +1,52 @@
 @extends('layouts.app')
+
 @section('content')
 
 <section>
-    <h1>INDEX</h1>
+    <h1 class="text-center" style="font-size: 1.5rem;">Lista piatti <br> <span class="text-danger">Deliveboo</span></h1>
     @foreach ($dishes as $dish)
-    <div class="d-flex">
-        <h3>{{ $dish->name}}</h3>
+        <div class="d-flex gap-3">
+            <ul>
+                <li class="mb-3">{{ $dish->name }}</li>
+                <div class="d-flex gap-4"> 
+                    <a href="{{ route('admin.dishes.show', $dish) }}" class="btn btn-outline-primary btn-sm"><i
+                            class="bi bi-binoculars me-1"></i>Visualizza</a>
+                    <button type="button" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal"
+                        data-bs-target="#exampleModal{{ $dish->id }}"><i class="bi bi-trash"></i>
+                        Elimina
+                    </button>
+                </div>
 
-        <a href="{{route('admin.dishes.show', $dish)}}" class="btn btn-primary">Show</a>
+            </ul>
 
-        <form action="{{route('admin.dishes.destroy', $dish)}}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">Elimina</button>
-        </form>
-    </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal{{ $dish->id }}" tabindex="-1"
+                aria-labelledby="exampleModalLabel{{ $dish->id }}" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel{{ $dish->id }}">Eliminazione Piatto</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <!-- Optional: Add any additional information here -->
+                            Sicuro di voler eliminare il piatto ("{{ $dish->name }}")?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                            <form action="{{ route('admin.dishes.destroy', $dish) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Elimina definitivamente</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     @endforeach
-
 </section>
 
 @endsection
--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,26 +1,50 @@
 @extends('layouts.app')
 @section('content')
-    <section>
-        <h1>SHOW</h1>
-        <h3>{{ $dish->id }}</h3>
-        <h3>{{ $dish->name }}</h3>
-        <h3>{{ $dish->description_ingredients }}</h3>
-        <h3>{{ $dish->price }}</h3>
-        
-        <!-- Check Boolean -->
-        @if ($dish->visible === 1)
-            <h3>Visibile</h3>
-        @else
-           <h3>Non visibile</h3> 
-        @endif
+<section>
+    <h1 class="text-center" style="font-size: 1.5rem;">Dettaglio prodotto <br> <span
+            class="text-danger">Deliveboo</span></h1>
+    <ul class="lh-lg">
+        <li><strong>Identificativo prodotto</strong>: {{ $dish->id }}</li>
+        <li><strong>Nome</strong>: {{ $dish->name }}</li>
+        <li><strong>Descrizione ingredienti</strong>: {{ $dish->description_ingredients }}</li>
+        <li><strong>Prezzo</strong>: {{ $dish->price }} €</li>
+    </ul>
 
-        <h3>{{ $dish->created_at}}</h3>
-        <h3>{{ $dish->updated_at}}</h3>
 
-        <form action="{{route('admin.dishes.destroy', $dish)}}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">Elimina</button>
-        </form>
-    </section>
+    <!-- Check Boolean -->
+    @if ($dish->visible === 1)
+        <h3>Visibile</h3>
+    @else
+        <h3>Non visibile</h3>
+    @endif
+
+    <h3>{{ $dish->created_at}}</h3>
+    <h3>{{ $dish->updated_at}}</h3>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminazione Piatto</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Sicuro di voler eliminare il piatto ("{{ $dish->name }}")?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                    <form action="{{route('admin.dishes.destroy', $dish)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Elimina</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Eliminazione piatto
+    </button>
+</section>
 @endsection
