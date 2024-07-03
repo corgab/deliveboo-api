2332f34d59e82b56f3186b1c015c31b77d81313f
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-07-03 16:04:50 +0200

    ADD: favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "icon" type = "image/png" href="{{ Vite::asset('resources/img/logo-icon.png')}}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
