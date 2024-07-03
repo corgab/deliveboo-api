f15d9440c3e80fd2092152812fbf7fad5ba3bef6
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-07-03 17:23:50 +0200

    RECAFTOR: Logo resize to 110px.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="main-header px-1 py-2">
     <div class="logo text-center">
         <a class="nav-link" href="{{url('/') }}">
-            <img  class="logo-img" src="{{ Vite::asset('resources/img/logo.png')}}" alt="Logo">
+            <img  class="logo-img" style="width: 110px" src="{{ Vite::asset('resources/img/logo.png')}}" alt="Logo">
         </a>
     </div>
 
