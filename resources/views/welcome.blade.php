d532796c4dea6131dcba729b86b6b4d1b02fe063
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-07-04 16:02:40 +0200

    REFACT: removed css style inline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,6 @@
         <h1 class="mb-3 text-center lh-base">Benvenuto nella piattaforma <br> <span class="text-danger">DELIVEBOO ADMIN</span>!</h1>
         <p class="fs-5 text-center">Gestisci il tuo ristorante da casa tua, con efficienza e comodità.</p>
     </div>
-    <img style="width: 100%; border-radius: 15px; margin-top: 35px" src="https://res.cloudinary.com/dqqmwn2ec/image/upload/v1720002356/pexels-mariana-kurnyk-844465-1775043_lobims.jpg" alt="">
+    <img class="home-page-foto" src="https://res.cloudinary.com/dqqmwn2ec/image/upload/v1720002356/pexels-mariana-kurnyk-844465-1775043_lobims.jpg" alt="">
 </div>
 @endsection
