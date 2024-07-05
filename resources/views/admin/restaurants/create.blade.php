84a91b64282757f6513bd1f720a8a56c3a0fb07a
gab <corbanigabriele@gmail.com>
2024-07-05 15:38:02 +0200

    ADD: types error stamp

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -19,12 +19,16 @@
         </div>
         <div class="mb-3">
             <label for="types" class="form-label"><strong>* Tipologie</strong>:</label>
+            @error('type_id')
+            <div class="invalid-feedback d-block">
+                {{ $message }}
+            </div>
+            @enderror
             <div class="form-check d-flex flex-wrap">
                 @foreach ($types as $type)
                     <div class="col-2">
-                        <input @checked(in_array($type->id, old('type_id', []))) name="type_id[]" type="checkbox" value="{{ $type->id }}"
-                            id="type-{{ $type->id }}">
-                        <label for="type-{{ $type->id }}">{{ $type->name }}</label>
+                        <input @checked(in_array($type->id, old('type_id', []))) class="form-check-input @error('type_id') is-invalid @enderror" name="type_id[]" type="checkbox" value="{{ $type->id }}" id="type-{{ $type->id }}">
+                        <label class="form-check-label" for="type-{{ $type->id }}">{{ $type->name }}</label>
                     </div>
                 @endforeach
             </div>
