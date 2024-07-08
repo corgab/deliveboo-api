295be5ab59bcf4665aa43e79e2e0f8b56f9f597f
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-07-08 11:19:00 +0200

    REFACT: removed my.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-  <footer class="d-flex flex-column flex-md-row align-items-center py-3 my-4 border-top">
+  <footer class="d-flex flex-column flex-md-row align-items-center py-4 border-top">
     <p class="col-md-3 mb-3 mb-md-0 text-body-secondary">&copy; 2024 Deliveboo, Inc</p>
 
     <ul class="nav col-md-9 justify-content-center justify-content-md-end">
