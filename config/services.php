35acbdcfe1e894e8de46ac69f86543f50401e717
corgab <corbanigabriele@gmail.com>
2024-07-09 12:43:30 +0200

    .

--- config/services.php
+++ config/services.php
@@ -32,10 +32,10 @@ return [
     ],
 
     'braintree' => [
-    'environment' => env('BRAINTREE_ENV'),
-    'merchantId' => env('BRAINTREE_MERCHANT_ID'),
-    'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
-    'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
-],
+        'environment' => env('BRAINTREE_ENV'),
+        'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+        'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+        'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
+    ],
 
 ];
