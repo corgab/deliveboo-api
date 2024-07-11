30992f444e5d026b8dc54df6d793d3d43f5005e5
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-07-11 16:08:37 +0200

    REFACT. Remove social network icon items.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,12 +2,6 @@
   <footer class="d-flex flex-column flex-md-row align-items-center py-4 border-top">
     <p class="col-md-3 mb-3 mb-md-0 text-body-secondary">&copy; 2024 Deliveboo, Inc</p>
 
-    <div class="d-flex gap-4">
-      <i class="bi bi-facebook"></i>
-      <i class="bi bi-instagram"></i>
-      <i class="bi bi-twitter-x"></i>
-    </div>
-
     <ul class="nav col-md-9 justify-content-center justify-content-md-end align-items-center">
       <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">Home</a></li>
       <li class="nav-item"><a href="/admin" class="nav-link px-2 text-body-secondary">Il tuo ristorante</a></li>
