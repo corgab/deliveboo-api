d07c288539a49d5cbbaba53b9dc1a5fb94c56891
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-07-11 11:23:24 +0200

    REFACT. Add Chi siamo and social icon,

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,12 +2,19 @@
   <footer class="d-flex flex-column flex-md-row align-items-center py-4 border-top">
     <p class="col-md-3 mb-3 mb-md-0 text-body-secondary">&copy; 2024 Deliveboo, Inc</p>
 
+    <div class="d-flex gap-3">
+      <i class="bi bi-facebook">Facebook</i>
+      <i class="bi bi-instagram">Instagram</i>
+      <i class="bi bi-twitter-x">X / Twitter</i>
+    </div>
+
     <ul class="nav col-md-9 justify-content-center justify-content-md-end">
       <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">Home</a></li>
       <li class="nav-item"><a href="/admin" class="nav-link px-2 text-body-secondary">Il tuo ristorante</a></li>
       <li class="nav-item"><a href="/admin/dishes" class="nav-link px-2 text-body-secondary">Il tuo menu</a></li>
       <li class="nav-item"><a href="/admin/dishes/create" class="nav-link px-2 text-body-secondary">Aggiungi un
           piatto</a></li>
-    </ul> 
+      <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">Chi siamo</a></li>
+    </ul>
   </footer>
 </div>
