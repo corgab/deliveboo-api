068ac428b41c19d1bc89db42595f13b979abdb27
corgab <corbanigabriele@gmail.com>
2024-07-14 23:57:55 +0200

    -

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <div class="container mt-2 pb-5">
-    <div class="row">
-        <h1 class="mb-3 text-center lh-base">Benvenuto nella piattaforma <br> <span class="text-danger">DELIVEBOO ADMIN</span>!</h1>
-        <p class="fs-5 text-center">Gestisci il tuo ristorante da casa tua, con efficienza e comodità.</p>
+    <h1 class="mb-3 text-center lh-base">Benvenuto nella piattaforma <br> <span class="text-danger">DELIVEBOO ADMIN</span>!</h1>
+    <p class="fs-5 text-center">Gestisci il tuo ristorante da casa tua, con efficienza e comodità.</p>
+    <div class="d-flex justify-content-center">
+        <img class="home-page-foto col-6" src="https://res.cloudinary.com/dqqmwn2ec/image/upload/v1720002356/pexels-mariana-kurnyk-844465-1775043_lobims.jpg" alt="" style="width: 800px">
     </div>
-    <img class="home-page-foto" src="https://res.cloudinary.com/dqqmwn2ec/image/upload/v1720002356/pexels-mariana-kurnyk-844465-1775043_lobims.jpg" alt="">
 </div>
 @endsection
