c2d9d1079fa922f0585753ac7e1865cefa524a67
Davide-Morellato <morellato.davide@yahoo.it>
2024-07-16 09:21:37 +0200

    ADD: @else img dishes show

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -4,19 +4,22 @@
     <div class="container">
         <h1 class="text-center mb-5 text-success">Dettaglio Piatto</h1>
         @if($dish->thumb)
+        <ul class="lh-lg">
+            <li>
+                <img class="img-dish mb-3 mt-5" src="{{ asset('storage/' . $dish->thumb) }}" alt="Foto Piatto">
+            </li>
+            @else
             <ul class="lh-lg">
-                <li>
-                    <img class="img-dish mb-3 mt-5" src="{{ asset('storage/' . $dish->thumb) }}" alt="Foto Piatto">
-                </li>
+                <img v-else class="img-dish mb-3 mt-5" src="{{ asset('storage/images/logo.png') }}" alt="Foto Piatto">
         @endif
             <li class="fs-show mb-3"><strong>Nome piatto</strong>: {{ $dish->name }}</li>
             <li class="fs-show mb-3"><strong>Descrizione ingredienti</strong>: {{ $dish->description_ingredients }}</li>
             <li class="fs-show mb-3"><strong>Prezzo</strong>: {{ $dish->price }} €</li>
             <li>
                 @if ($dish->visible == true)
-                    <h3 class="h5 text-success">Visibile</h3>
+                <h3 class="h5 text-success">Visibile</h3>
                 @else
-                    <h3 class="h5 text-danger">Non visibile</h3>
+                <h3 class="h5 text-danger">Non visibile</h3>
                 @endif
             </li>
         </ul>
@@ -40,9 +43,9 @@
                         <button type="submit" class="btn btn-danger">Elimina</button>
                     </form>
                     {{-- <form action="{{ route('admin.dishes.permanentDelete', $dish->id) }}" method="POST" style="display:inline-block;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Elimina definitivamente</button>
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Elimina definitivamente</button>
                     </form> --}}
                 </div>
             </div>
@@ -51,8 +54,7 @@
 
     {{-- Da togliere --}}
     <div class="d-flex flex-column flex-md-row justify-content-center justify-content-md-start align-items-center gap-3 mt-4">
-        <button type="button" class="btn btn-sm btn-outline-danger mb-2 mb-md-0 me-md-2" data-bs-toggle="modal"
-            data-bs-target="#exampleModal"><i class="bi bi-trash-fill"></i>
+        <button type="button" class="btn btn-sm btn-outline-danger mb-2 mb-md-0 me-md-2" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-trash-fill"></i>
             Eliminazione piatto
         </button>
         <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-outline-success btn-sm"><i class="bi bi-pen-fill"></i>
